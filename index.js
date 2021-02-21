626462f554845eaf3f45b6e4f5fe98eea182b1c4
santhoshjanan <santhoshj@gmail.com>
2021-02-21 18:43:17 +0530

    cors

--- index.js
+++ index.js
@@ -3,12 +3,19 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var moment = require('moment');
+var cors = require('cors')
 
 const port = 3000
+
+var corsOptions = {
+    origin: 'http://localhost:8080',
+    optionsSuccessStatus: 200 // For legacy browser support
+}
 
 var app = express();
 
 app.use(logger('dev'));
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
